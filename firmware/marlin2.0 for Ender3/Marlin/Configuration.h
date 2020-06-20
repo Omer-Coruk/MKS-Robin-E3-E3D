aa1146ec7db8b5e001456730da6754ece8356175
Omer Coruk <omer.coruk@bubblesdiving.com>
2020-06-21 02:32:04 +0300

    Update Configuration.h

--- firmware/marlin2.0 for Ender3/Marlin/Configuration.h
+++ firmware/marlin2.0 for Ender3/Marlin/Configuration.h
@@ -927,7 +927,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44, -16, -2.3 }
+#define NOZZLE_TO_PROBE_OFFSET { -44, -16, -2.4 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 5
